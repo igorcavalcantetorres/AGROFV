b0fd7e9ca3bb4477c0e4305e704481926c0ff76c
Igor Torres <igor.torres@ceca.ufal.br>
2024-12-03 14:55:44 -0300

    Criação do plot automático com irradiância e temperatura

--- executavel.py
+++ executavel.py
@@ -5,8 +5,9 @@ import matplotlib.pyplot as plt
 from datetime import date, timedelta, datetime
 import csv
 import time
+from matplotlib.ticker import MaxNLocator
 
-caminho_do_arquivo_1 = r'C:\Users\i5\Documents\GitProjetos\AGROFV\CR10X_SAF_1.dat'
+caminho_do_arquivo_1 = r'C:\Users\igorc\Documents\GitProjetos\AGROFV\CR10X_SAF_1.dat'
 dados = pd.read_csv(caminho_do_arquivo_1, sep=',', header=[0]) 
 coluna_2=dados['h']
     
@@ -50,26 +51,38 @@ irradiacao=dados['f']
 temperatura=dados['g']
 data_frame=pd.DataFrame(resultados, columns=["Dia Juliano","Data","Hora"])
 data_frame["Irradiacao"]=irradiacao
-data_frame.to_csv(r'C:\Users\i5\Documents\GitProjetos\AGROFV\resultados.csv', index=False, sep=',', header=True)
-
-plt.figure(figsize=(10, 6))
-
-plt.plot(data_frame['Hora'], data_frame['Irradiacao'], color='blue', alpha=0.5)
-plt.xlabel('Hora')
-plt.ylabel('Irradiação - W/m²')
-plt.title('Irradiação no dia')
-plt.draw()
+data_frame["Temperatura"]=temperatura
+data_frame.to_csv(r'C:\Users\igorc\Documents\GitProjetos\AGROFV\resultados.csv', index=False, sep=',', header=True)
 
 for i in range(0, len(data_frame), bloco):
-        plt.clf()
-        plt.figure(figsize=(10, 6))
-        plt.plot(data_frame['Hora'][i:i+bloco], data_frame['Irradiacao'][i:i+bloco], color='blue')
-        legenda = data_frame['Data'][i] 
-        plt.legend([f'Dia {legenda}'], loc='upper left')
-        plt.xlabel('Hora')
-        plt.ylabel('Irradiação')
-        plt.title(f'Irradiação {i+bloco}')   
-        plt.show()  
-        time.sleep(2)
+    plt.figure(figsize=(12, 8))
+    
+    # Subplot 1: Gráfico de Irradiação
+    plt.subplot(2, 1, 1)  # 2 linhas, 1 coluna, primeiro gráfico
+    plt.plot(data_frame['Hora'][i:i+bloco], data_frame['Irradiacao'][i:i+bloco], color='blue')
+    legenda = data_frame['Data'][i] 
+    plt.legend([f'Dia {legenda}'], loc='upper left')
+    plt.xlabel('Hora')
+    plt.ylabel('Irradiação - W/m²')
+    plt.title(f'Irradiação do dia {data_frame["Data"][i]}')
+    plt.xticks(rotation=45)  # Rotacionar os rótulos para melhor visualização
+    plt.gca().xaxis.set_major_locator(MaxNLocator(nbins=24))
+    
+    # Subplot 2: Gráfico de Temperatura
+    plt.subplot(2, 1, 2)  # 2 linhas, 1 coluna, segundo gráfico
+    plt.plot(data_frame['Hora'][i:i+bloco], data_frame['Temperatura'][i:i+bloco], color='red')
+    plt.legend([f'Dia {legenda}'], loc='upper left')
+    plt.xlabel('Hora')
+    plt.ylabel('Temperatura - °C')
+    plt.title(f'Temperatura do dia {data_frame["Data"][i]}')
+    plt.xticks(rotation=45)  # Rotacionar os rótulos para melhor visualização
+    plt.gca().xaxis.set_major_locator(MaxNLocator(nbins=24))
+   
+    # Ajustando o layout para evitar sobreposição
+    plt.tight_layout()
+    plt.show()
+
+    # Pausando por 2 segundos antes de exibir o próximo gráfico
+    time.sleep(2)
         
  
